6586c933b9761da99cbec39410ee9bc8cd9cee86
Javier González Martín <jgm_desarrollo@hotmail.com>
2024-06-12 19:23:44 +0200

    Incluidos botones de contacto

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 addEventListener('DOMContentLoaded',()=>{
  const menu_btn = document.querySelector('#navbtn');
-    if (menu_btn){
-        menu_btn.addEventListener('click', ()=>{
-            const menu_items = document.querySelector('#navitems');
+ if (menu_btn){
+     menu_btn.addEventListener('click', ()=>{
+        const menu_items = document.querySelector('#navitems');
             menu_items.classList.toggle('showmenu');
         })
-     }
+    }
 })
+
+let textoemail = document.getElementById('emailvalor').innerHTML;
+const CopiarEmail = async () => {
+   try {
+    await navigator.clipboard.writeText(textoemail);
+    console.log('Contenido copiado al portapapeles:'+textoemail);
+  } catch (err) {
+    console.error('Error al copiar: ', err);
+  }
+}
+
